c446ae61af90ec00ea7dd5dcf9c81414bf79caed
DVKuklin <dvkuklin@yandex.ru>
2023-04-03 16:49:42 +0300

    Edit getDistrictId in Clinic

--- src/Docdoc/Models/Clinic.php
+++ src/Docdoc/Models/Clinic.php
@@ -72,9 +72,13 @@ class Clinic extends Model
     /**
      * @return int
      */
-    public function getDistrictId(): int
+    public function getDistrictId(): ?int
     {
-        return $this->get('DistrictId');
+        $district_id = $this->get('DistrictId');
+        if ($district_id === 'null') {
+            return null;
+        }
+        return $district_id;
     }
 
     /**
